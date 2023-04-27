30b04cb36ae4d11e496cc2e7db8dfb8b1b208391
Jason <jasonkodama@gmail.com>
2023-04-27 13:24:40 -0600

    added functioning  prompts

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const inqurier = require('inquirer');
-const shapes = require('shapes');
+const inquirer = require('inquirer');
+const fs = require('fs')
+
+//const shapes = require('shapes');
 
 inquirer 
-    .prompts ({
+    .prompt ([{
         type: 'input',
         message: 'Pick any 3 letters for your logo.',
         name: 'text',
-    }
+    },
     {
         type: 'input',
         message: 'What color would you like the text?',
         name: 'text-color',
-    }
+    },
     {
-        type: 'input',
+        type: 'list',
         message: 'Pick a shape for your logo',
         name: 'shape',
-        choice: 'circle, triangle, square',
-    }
+        choices: ['circle', 'triangle', 'square'],
+    },
     {
         type: 'input',
         message: 'What color would you like the shape?',
         name: 'shape-color',
     }
-    )
+])
