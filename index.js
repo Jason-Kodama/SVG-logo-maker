c69a320d409aecac3a3e59f5831ad963e565d783
Jason <jasonkodama@gmail.com>
2023-04-29 14:19:12 -0600

    added functionality to shape render

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
+const shapes = require('./lib/shapes');
 
-//const shapes = require('shapes');
+//const generateShapes =
 
-inquirer 
-    .prompt ([{
+inquirer
+    .prompt([{
         type: 'input',
         message: 'Pick any 3 letters for your logo.',
         name: 'text',
@@ -12,7 +13,7 @@ inquirer
     {
         type: 'input',
         message: 'What color would you like the text?',
-        name: 'text-color',
+        name: 'textColor',
     },
     {
         type: 'list',
@@ -23,6 +24,26 @@ inquirer
     {
         type: 'input',
         message: 'What color would you like the shape?',
-        name: 'shape-color',
+        name: 'shapeColor',
     }
-])
+    ])
+    .then((answers) => {
+        console.log(answers)
+        let shape 
+        if(answers.shape === 'circle') {
+            shape = new shapes.Circle()
+        }else if(answers.shape ==='triangle'){
+            shape = new shapes.Triangle()
+        }else if(answers.shape === 'square'){
+            shape = new shapes.Square()
+        }
+        const theSvg = `<svg width="300" height="200" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" fill=${answers.shapeColor} stroke="#000" stroke-linecap="round">
+${shape.render()}
+<text x="50%" y="50%" font-size = "1.7em" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+</svg>`
+        return fs.promises.writeFile('Logo.svg', theSvg)
+    })
+    .then(() => {
+        console.log('generated file')
+    })
+    
